10cc6cc7e4f7220d6ac14ce1c4803784ff3099fa
unknown <LIU.Nian@sindyware.com>
2017-11-06 09:43:53 +0800

    add project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1 v-text="title"></h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function(){
+    return {
+      title:"liun demo"
+    }
+  },
+  data2 (){
+    return {
+      title:"liun demo"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="show('hello')">say hello</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'demo',
+  data: function() {
+    return {
+      msg: '12345'
+    }
+  },
+  methods:{
+    show:function(msg){
+      alert(msg)
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
